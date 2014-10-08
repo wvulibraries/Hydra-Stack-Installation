d71899ed3fa9c3688cccb419f5119c4724cc05f1
Michael Bond <mbond@the-forgotten.org>
2014-10-07 20:44:00 -0400

    Export ENV variables just prior to expects script.

--- install/install_fedora.sh
+++ install/install_fedora.sh
@@ -7,8 +7,10 @@ if [ "$EUID" -ne "0" ] ; then
 fi
 
 # set environment variables for fedora
-echo 'PATH=$PATH:$FEDORA_HOME/server/bin:$FEDORA_HOME/client/bin' | tee -a /etc/profile.d/fedora.sh
 source /etc/environment
+export FEDORA_INSTALL_DIR
+export HYDRA_INSTALL_DIR
+echo "PATH=$PATH:$FEDORA_HOME/server/bin:$FEDORA_HOME/client/bin" | tee -a /etc/profile.d/fedora.sh
 source /etc/profile.d/fedora.sh
 
 # check the output of "echo $PATH"
